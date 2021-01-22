67883000a0e073fd6744af951140cd7426f7d778
André Almeida <andalvalmeida@gmail.com>
2021-01-22 15:34:10 -0300

    Create appointment repository

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,16 @@
-import { isEqual, parseISO, startOfHour } from 'date-fns';
+import { parseISO, startOfHour } from 'date-fns';
 import { Router } from 'express';
-import Appointment from '../models/Appointment';
+import AppointmentRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-const appointments: Appointment[] = [];
+const appointmentRepository = new AppointmentRepository();
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(appointment.date, parsedDate),
+  const findAppointmentInSameDate = appointmentRepository.findByDate(
+    parsedDate,
   );
 
   if (findAppointmentInSameDate) {
@@ -20,9 +19,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
-
-  appointments.push(appointment);
+  const appointment = appointmentRepository.create(provider, parsedDate);
 
   return response.status(201).json(appointment);
 });
