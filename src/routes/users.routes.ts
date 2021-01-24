f0fbee3c96d1f533e3c0e77854c8afd254921156
André Almeida <andalvalmeida@gmail.com>
2021-01-24 20:21:32 -0300

    Update user avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -32,7 +33,21 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    response.json({ ok: true });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFileName: request.file.filename,
+      });
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password: userPassword, ...userWithoutPassword } = user;
+
+      return response.json(userWithoutPassword);
+    } catch (err) {
+      return response.status(400).json({ message: err.message });
+    }
   },
 );
 
